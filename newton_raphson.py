f853d5464fcc3f5a98730f9805fb42fdd59e1cde
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2022-11-22 22:00:05 +1300

    Update newton_raphson.py

--- newton_raphson.py
+++ newton_raphson.py
@@ -11,6 +11,10 @@ max_iteration = 10
 
 with open('data/call_options.pkl', 'rb') as f:
     s, k, t, r, c = pickle.load(f)
+# Assumptions:
+# *Observations that do not satisfy assumptions are picked out in pre-processing.*
+# 1. All values (s, k, t, r, c) are positive.
+# 2. There is a root, i.e. call_value(..., sigma=0) < 0
 
 n = s.shape[0]
 vega = np.zeros((n, max_iteration))
@@ -18,9 +22,9 @@ sigma = np.zeros((n, max_iteration + 1))
 sigma[:, 0] = np.sqrt(2 / t * np.abs(np.log(s / k) + r * t))
 for i in range(max_iteration):
     vega[:, i] = call_vega(s, k, t, r, sigma[:, i])
-    sigma[:, i + 1] = sigma[:, i] - (call_value(s, k, t, r, sigma[:, i]) - c) / vega[:, i]
     c_hat = call_value(s, k, t, r, sigma[:, i])
-    print('Iteration:', i + 1, 'MSE:', mean_squared_error(c, c_hat))
+    sigma[:, i + 1] = sigma[:, i] - (c_hat - c) / vega[:, i]
+    print('Iteration:', i + 1, 'MSE:', mean_squared_error(c, c_hat))  # MSE of the call option value
 
 with open('results/volatility_newton.pkl', 'wb') as f:
     pickle.dump(sigma[:, max_iteration], f)
